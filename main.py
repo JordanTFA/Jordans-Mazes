14b13614ce697f24ce03e41ad22d05b4e567a3ac
JordanTFA <Jordantfa@hotmail.co.uk>
2018-11-29 11:51:22 +0000

    Getting dimensions

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 from PIL import Image, ImageDraw
 
-def pickSize():
-    size = input("Pick size")
-    print(size)
-    return size;
+def getinput():
+    dimensions = input("Please enter your dimensions - separate by comma\n")
+    dimensions = dimensions.split(',')
+    return dimensions;
+
+def draw(x, y):
+    print(str(x) + " " + str(y))
 
 if __name__ == '__main__':
-    pickSize()
-
-    height = 600
-    width = 600
-    image = Image.new(mode='L', size=(height, width), color=255)
-
-    # Draw a line
-    draw = ImageDraw.Draw(image)
-    x = image.width / 2
-    y_start = 0
-    y_end = image.height
-    line = ((x, y_start), (x, y_end))
-    draw.line(line, fill=128)
-    del draw
-
-    image.show()
+    dimensions = getinput()
+    x = dimensions[0]
+    y = dimensions[1]
+    draw(x,y)
