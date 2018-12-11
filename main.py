2c8e629091dd000184f57feb915fdc5a32dbb3cd
JordanTFA <Jordantfa@hotmail.co.uk>
2018-12-11 23:50:18 +0000

    Not picking up visited tiles, idk why

--- main.py
+++ main.py
@@ -4,78 +4,87 @@ from random import choice ,randint, shuffle
 def makemaze(width, height):
 
     visited = []
-    popped = []
-    #visited.append([0,0])
-    print("Visited: " + str(visited))
+    print("D: [0,0]\n")
 
     def walk(x,y):
 
+        outOfBounds = []
+        v = []
         visited.append([x,y])
-        print(visited)
 
-        north = [x - 1, y]
-        east = [x, y + 1]
-        south = [x + 1, y]
-        west = [x, y - 1]
+        north = [x, y - 1]
+        east = [x + 1, y]
+        south = [x, y + 1]
+        west = [x - 1, y]
 
         directions = [north, east, south, west]
 
-        # for xx,yy in directions:
-        #     if xx < 0 or xx >= width or yy < 0 or yy >= height:
-        #         directions.remove([xx,yy])
-        #         print("Removing " + str([xx,yy]) + ", Directions left = " + str(directions))
+        for index in range(3,-1,-1):
+            xx,yy=directions[index]
+            if xx < 0 or xx >= width or yy < 0 or yy >= height:
+                del directions[index]
+                outOfBounds.append([xx,yy])
+                if [xx,yy] in visited:
+                    v.append([xx,yy])
 
         d = choice(directions)
-        while not inBounds(d):
-            print("Initial Out of bounds: " + str(d) + ", Directions: " + str(directions))
 
-            if len(directions) > 1:
-                directions.remove(d)
-                print("Removed: " + str(d))
-                d = choice(directions)
+        print("Out of Bounds: " + str(outOfBounds) +
+              "\nVisited tiles: " + str(v) +
+              "\nAvailable directions : " + str(directions) +
+              "\nD: " + str(d) +
+              "\n")
 
-            else:
-                print("Out of neighbours - Now we backtrack...")
-                popped.append(visited[-1])
-                visited.pop()
-                d = visited[-1]
-                print("New D: " + str(d))
-                walk(d[0], d[1])
 
-        while d in visited:
+        # d = choice(directions)
+        # while not inBounds(d):
+        #     print("Initial Out of bounds: " + str(d) + ", Directions: " + str(directions))
+        #
+        #     if len(directions) > 1:
+        #         directions.remove(d)
+        #         print("Removed: " + str(d))
+        #         d = choice(directions)
+        #
+        #     else:
+        #         print("Out of neighbours - Now we backtrack...")
+        #         popped.append(visited[-1])
+        #         visited.pop()
+        #         d = visited[-1]
+        #         print("New D: " + str(d))
+        #         walk(d[0], d[1])
+        #
+        # while d in visited:
+        #
+        #     #print("Visited: " + str(d))
+        #
+        #     if len(directions) > 1:
+        #         directions.remove(d)
+        #         d = choice(directions)
+        #
+        #     else:
+        #         print("Out of neighbours - Now we backtrack...")
+        #         visited.pop()
+        #         d = visited[-1]
+        #         print("New D: " + str(d))
+        #         walk(d[0], d[1])
+        #
+        #     if not inBounds(d):
+        #
+        #         if len(directions) > 1:
+        #             directions.remove(d)
+        #             print("Removed: " + str(d))
+        #             d = choice(directions)
+        #
+        #         else:
+        #             print("Out of neighbours - Now we backtrack...")
+        #             visited.pop()
+        #             d = visited[-1]
+        #             print("New D: " + str(d))
+        #             walk(d[0], d[1])
 
-            print("Visited: " + str(d))
+        #print("D: " + str(d))
 
-            if len(directions) > 1:
-                directions.remove(d)
-                d = choice(directions)
-
-            else:
-                print("Out of neighbours - Now we backtrack...")
-                popped.append(visited[-1])
-                visited.pop()
-                d = visited[-1]
-                print("New D: " + str(d))
-                walk(d[0], d[1])
-
-            if not inBounds(d):
-
-                if len(directions) > 1:
-                    directions.remove(d)
-                    print("Removed: " + str(d))
-                    d = choice(directions)
-
-                else:
-                    print("Out of neighbours - Now we backtrack...")
-                    popped.append(visited[-1])
-                    visited.pop()
-                    d = visited[-1]
-                    print("New D: " + str(d))
-                    walk(d[0], d[1])
-
-        print("D: " + str(d))
-
-        if (len(visited) + len(popped)) < width * height:
+        if len(visited) < (width * height):
             walk(d[0], d[1])
 
     walk(0,0)
