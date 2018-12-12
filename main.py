f80bd1883016ff67a617414127100765175b830b
JordanTFA <Jordantfa@hotmail.co.uk>
2018-12-12 06:31:55 +0000

    Condensed a lot, now works until dead end

--- main.py
+++ main.py
@@ -20,88 +20,39 @@ def makemaze(width, height):
         directions = [north, east, south, west]
 
         for index in range(3,-1,-1):
-            xx,yy=directions[index]
+            xx,yy = directions[index]
             if xx < 0 or xx >= width or yy < 0 or yy >= height:
                 del directions[index]
                 outOfBounds.append([xx,yy])
-                if [xx,yy] in visited:
-                    v.append([xx,yy])
 
-        d = choice(directions)
+            if [xx,yy] in visited:
+                v.append([xx,yy])
+                if directions:
+                    del directions[index]
 
-        print("Out of Bounds: " + str(outOfBounds) +
-              "\nVisited tiles: " + str(v) +
-              "\nAvailable directions : " + str(directions) +
-              "\nD: " + str(d) +
-              "\n")
+        # Valid tile - let's continue
+        if directions:
+            d = choice(directions)
 
+            print("Out of Bounds: ", outOfBounds)
+            print("Visited tiles: ", v)
+            print("Available directions : ", directions)
+            print("D: ", d)
 
-        # d = choice(directions)
-        # while not inBounds(d):
-        #     print("Initial Out of bounds: " + str(d) + ", Directions: " + str(directions))
-        #
-        #     if len(directions) > 1:
-        #         directions.remove(d)
-        #         print("Removed: " + str(d))
-        #         d = choice(directions)
-        #
-        #     else:
-        #         print("Out of neighbours - Now we backtrack...")
-        #         popped.append(visited[-1])
-        #         visited.pop()
-        #         d = visited[-1]
-        #         print("New D: " + str(d))
-        #         walk(d[0], d[1])
-        #
-        # while d in visited:
-        #
-        #     #print("Visited: " + str(d))
-        #
-        #     if len(directions) > 1:
-        #         directions.remove(d)
-        #         d = choice(directions)
-        #
-        #     else:
-        #         print("Out of neighbours - Now we backtrack...")
-        #         visited.pop()
-        #         d = visited[-1]
-        #         print("New D: " + str(d))
-        #         walk(d[0], d[1])
-        #
-        #     if not inBounds(d):
-        #
-        #         if len(directions) > 1:
-        #             directions.remove(d)
-        #             print("Removed: " + str(d))
-        #             d = choice(directions)
-        #
-        #         else:
-        #             print("Out of neighbours - Now we backtrack...")
-        #             visited.pop()
-        #             d = visited[-1]
-        #             print("New D: " + str(d))
-        #             walk(d[0], d[1])
+            print(str(visited[-1]) + " <-> " + str(d) + "\n")
 
-        #print("D: " + str(d))
+            if len(visited) < (width * height):
+                walk(d[0], d[1])
 
-        if len(visited) < (width * height):
-            walk(d[0], d[1])
-
+        # No neighbours - time to backtrack...
+        else:
+            print("No Neighbours")
+            visited.pop()
     walk(0,0)
 
     print("Finished: " + str(len(visited)) + " " + str(visited))
 
-def inBounds(d):
-
-    if d[0] < 0 or \
-            d[0] > width - 1 or \
-            d[1] < 0 or \
-            d[1] > height - 1:
-        return False
-    else:
-        return True
-
 if __name__ == '__main__':
 
-    width,height = 4,4
-    makemaze(width,height)
+    width, height = 4, 4
+    makemaze(width, height)
