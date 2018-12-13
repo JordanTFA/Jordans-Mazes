31db532e5d53a31e63224a1a6cc5e51d079dcc0f
JordanTFA <Jordantfa@hotmail.co.uk>
2018-12-13 06:10:06 +0000

    IT WORKS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from random import choice ,randint, shuffle
 def makemaze(width, height):
 
     visited = []
+    popped = []
+    result = []
     print("D: [0,0]\n")
 
     def walk(x,y):
@@ -25,7 +27,7 @@ def makemaze(width, height):
                 del directions[index]
                 outOfBounds.append([xx,yy])
 
-            if [xx,yy] in visited:
+            if [xx,yy] in visited or [xx,yy] in popped:
                 v.append([xx,yy])
                 if directions:
                     del directions[index]
@@ -33,24 +35,35 @@ def makemaze(width, height):
         # Valid tile - let's continue
         if directions:
             d = choice(directions)
+            result.append(str(visited[-1]) + " <-> " + str(d))
 
             print("Out of Bounds: ", outOfBounds)
             print("Visited tiles: ", v)
             print("Available directions : ", directions)
             print("D: ", d)
+            print("Result: ", result[-1])
+            print()
 
-            print(str(visited[-1]) + " <-> " + str(d) + "\n")
-
-            if len(visited) < (width * height):
+            if (len(visited) + len(popped)) < 15:
                 walk(d[0], d[1])
 
         # No neighbours - time to backtrack...
         else:
             print("No Neighbours")
+            print("Visited: ", visited)
+            print("Popped: ", popped)
+            popped.append(visited[-1])
             visited.pop()
+            d = visited[-1]
+            visited.pop()
+            print("D: ", d)
+            print()
+
+            walk(d[0], d[1])
     walk(0,0)
 
-    print("Finished: " + str(len(visited)) + " " + str(visited))
+    for r in result:
+        print(r)
 
 if __name__ == '__main__':
 
