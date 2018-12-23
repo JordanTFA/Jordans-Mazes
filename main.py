e1b1feef0731f76c8aca71ef6c90abde4f176586
Jordan Aitken <Jordantfa@hotmail.co.uk>
2018-12-23 00:02:09 +0000

    Injecting data into grid list

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+<<<<<<< HEAD
+from random import choice, randint, shuffle
+=======
 from random import choice ,randint, shuffle
+>>>>>>> origin/master
+
 
 def makemaze(width, height):
-
     visited = []
     popped = []
     result = []
     print("D: [0,0]\n")
 
-    def walk(x,y):
+    def walk(x, y):
 
         outOfBounds = []
         v = []
-        visited.append([x,y])
+        visited.append([x, y])
 
         north = [x, y - 1]
         east = [x + 1, y]
@@ -20,14 +24,14 @@ def makemaze(width, height):
 
         directions = [north, east, south, west]
 
-        for index in range(3,-1,-1):
-            xx,yy = directions[index]
+        for index in range(3, -1, -1):
+            xx, yy = directions[index]
             if xx < 0 or xx >= width or yy < 0 or yy >= height:
                 del directions[index]
-                outOfBounds.append([xx,yy])
+                outOfBounds.append([xx, yy])
 
-            if [xx,yy] in visited or [xx,yy] in popped:
-                v.append([xx,yy])
+            if [xx, yy] in visited or [xx, yy] in popped:
+                v.append([xx, yy])
                 if directions:
                     del directions[index]
 
@@ -59,10 +63,54 @@ def makemaze(width, height):
             print()
 
             walk(d[0], d[1])
-    walk(0,0)
+
+    walk(0, 0)
 
     for r in result:
         print(r)
+<<<<<<< HEAD
+
+
+def drawmaze(width, height):
+    hw = "|  "
+    hp = "   "
+
+    vw = "+--"
+    vp = "+  "
+
+    row = ""
+
+    # print()
+    # print(("+--" * width) + "+")
+    # grid[width][height]
+    row += hw + hw + hp + hp + hp + hw + "\n"
+    row += vp + vp + vw + vp + vp + vp + "\n"
+    # print(row)
+
+    grid = []
+    for row in range(height): grid += [["t"] * width]
+
+    grid[2][2] = "E"
+
+    msg = "\n"
+    for row in grid:
+        for t in row:
+            msg += t
+        msg += "\n"
+
+    print(msg)
+    # print(("+--" * width) + "+")
+    # for i in range(height):
+    # print (horz[i])
+    # if i < len(vert):
+    # print(vert[i])
+    # for i in range(height):
+    # print(("+--" * width) + "+")
+    # print("|  " * (width + 1))
+
+    # Print floor
+    # print(("+--" * width) + "+")
+=======
     
 def drawmaze(width, height):
     
@@ -102,6 +150,7 @@ def drawmaze(width, height):
 
     #Print floor
     #print(("+--" * width) + "+")
+>>>>>>> origin/master
 
     print()
     print("+--+--+--+--+--+")
@@ -113,8 +162,17 @@ def drawmaze(width, height):
     print("+  +  +  +  +  +")
     print("|        |  |  |")
     print("+--+--+--+--+--+")
+<<<<<<< HEAD
+
+
+if __name__ == '__main__':
+    width, height = 5, 4
+    makemaze(width, height)
+    drawmaze(width, height)
+=======
 if __name__ == '__main__':
 
     width, height = 5,4
     makemaze(width, height)
     drawmaze(width, height)
+>>>>>>> origin/master
